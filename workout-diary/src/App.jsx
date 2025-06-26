e553f3594d67d52b0e50146cfbb179e12d720831
Anastasia <kunitskaiaanastasia@gmail.com>
2025-06-26 13:41:46 +0300

    Initial Vite project setup

---
+++ workout-diary/src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+      <h1>Workout Diary</h1>
+      <p>Дневник тренировок</p>
+    </Container>
+  );
+}
+
+export default App;
